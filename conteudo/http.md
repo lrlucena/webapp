4dde0b0fcf83a635b15f232d458bc46bd99e259c
Leonardo Lucena <leonardo.lucena@ifrn.edu.br>
2016-05-25 18:49:37 -0300

    Protocolo HTTP

---
+++ conteudo/http.md
@@ -0,0 +1,8 @@
+# Protocolo HTTP
+
+
+[![Aplicações Web](https://img.youtube.com/vi/RsQ1tFLwldY/0.jpg)](https://www.youtube.com/watch?v=RsQ1tFLwldY)
+
+[![Engenharia Web](https://img.youtube.com/vi/ADQ_rhefgEk/0.jpg)](https://www.youtube.com/watch?v=ADQ_rhefgEk)
+
+[![Engenharia Web](https://img.youtube.com/vi/sxiRFwQ1RJ4/0.jpg)](https://www.youtube.com/watch?v=sxiRFwQ1RJ4)
--- exemplos/agenda1/app/controllers/HomeController.scala
+++ exemplos/agenda1/app/controllers/HomeController.scala
@@ -8,7 +8,7 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject() extends Controller {
 
-  private def texto(campo: String)(implicit request: Request[AnyContent]) = 
+  private def texto(campo: String)(implicit request: Request[AnyContent]) =
     request.body.asFormUrlEncoded.map {
       a => a(campo)(0)
     }.getOrElse("")
@@ -19,14 +19,14 @@ class HomeController @Inject() extends Controller {
   None        =>  ""
 
   Option[Map[String, Seq[String]]]
-*/  
-  private def inteiro(campo: String)(implicit request: Request[AnyContent]) = 
+*/
+  private def inteiro(campo: String)(implicit request: Request[AnyContent]) =
     texto(campo).toInt
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
   def todos(implicit request: Request[AnyContent]) = {
     val campos = request.body.asFormUrlEncoded.getOrElse(Map())
     val lista = for((chave,valor) <- campos) yield {
